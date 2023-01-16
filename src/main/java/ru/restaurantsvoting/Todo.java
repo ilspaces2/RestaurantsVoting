9bbd8cfe6eda900e14f6ccb05a90278a854c1022
I_M <moskva.88@mail.ru>
2023-01-16 23:43:49 +0300

    todo_1

---
+++ src/main/java/ru/restaurantsvoting/Todo.java
@@ -0,0 +1,17 @@
+package ru.restaurantsvoting;
+
+public class Todo {
+    /*
+TODO
+   1. update with current email     +
+   2. toString user and userdto
+   3. check all controllers     +
+   4. check security config?    +
+   5. check exceptions and handler
+   6. uniq restaurant
+   7. lombok all classes    -+
+   8. documents api
+   9. tests
+
+ */
+}
