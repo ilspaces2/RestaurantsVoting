b3f3c33b7167fdc00702367d1d9626c42e06ce2f
I_M <moskva.88@mail.ru>
2023-02-08 23:58:19 +0300

    AdminController_add_tests_part_2

--- src/test/java/ru/restaurantsvoting/controller/AdminControllerTest.java
+++ src/test/java/ru/restaurantsvoting/controller/AdminControllerTest.java
@@ -4,15 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import ru.restaurantsvoting.dto.UserDto;
-import ru.restaurantsvoting.model.Role;
-import ru.restaurantsvoting.model.User;
-
-import java.util.List;
-import java.util.Set;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.restaurantsvoting.UserTestData.*;
 
 class AdminControllerTest extends AbstractControllerTest {
 
@@ -21,53 +16,66 @@ class AdminControllerTest extends AbstractControllerTest {
     void findAll() throws Exception {
         preform(MockMvcRequestBuilders.get("/admin"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(objectMapper.writeValueAsString(getUsersList())));
+                .andExpect(content().string(objectMapper.writeValueAsString(getUsers())));
     }
 
     @Test
     @WithMockUser(value = "admin@admin.com", password = "admin", roles = {"ADMIN"})
     void get() throws Exception {
-        preform(MockMvcRequestBuilders.get("/admin/{id}", 1))
+        preform(MockMvcRequestBuilders.get("/admin/{id}", USER_ID))
                 .andExpect(status().isOk())
-                .andExpect(content().string(objectMapper.writeValueAsString(getUsersList().get(0))));
+                .andExpect(content().string(objectMapper.writeValueAsString(user)));
     }
 
     @Test
     @WithMockUser(value = "admin@admin.com", password = "admin", roles = {"ADMIN"})
+    void whenGetAndUserNotFoundThenThrowException() throws Exception {
+        preform(MockMvcRequestBuilders.get("/admin/{id}", BAD_ID))
+                .andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    @WithMockUser(value = "admin@admin.com", password = "admin", roles = {"ADMIN"})
     void delete() throws Exception {
-        preform(MockMvcRequestBuilders.delete("/admin/{id}", 2))
+        preform(MockMvcRequestBuilders.delete("/admin/{id}", USER_ID))
                 .andExpect(status().isNoContent());
-        preform(MockMvcRequestBuilders.get("/admin/{id}", 2))
+        preform(MockMvcRequestBuilders.get("/admin/{id}", USER_ID))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(value = "admin@admin.com", password = "admin", roles = {"ADMIN"})
+    void whenDeleteAndUserNotFoundThenThrowException() throws Exception {
+        preform(MockMvcRequestBuilders.delete("/admin/{id}", BAD_ID))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     @WithMockUser(value = "admin@admin.com", password = "admin", roles = {"ADMIN"})
     void update() throws Exception {
-        UserDto userDto = new UserDto("user@user.com", "user1");
-        userDto.setName("new name");
-        User updated = getUsersList().get(1);
-        updated.setName(userDto.getName());
-        preform(MockMvcRequestBuilders.put("/admin/{id}", 2)
+        preform(MockMvcRequestBuilders.put("/admin/{id}", USER_ID)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(userDto)))
+                .content(objectMapper.writeValueAsString(getUpdatedUserDto())))
                 .andExpect(status().isNoContent())
-                .andExpect(content().string(objectMapper.writeValueAsString(updated)));
+                .andExpect(content().string(objectMapper.writeValueAsString(getUpdatedUser())));
     }
 
-    private List<User> getUsersList() {
-        User admin = new User();
-        admin.setId(1);
-        admin.setName("admin");
-        admin.setEmail("admin@admin.com");
-        admin.setVoted(false);
-        admin.setRoles(Set.of(Role.ADMIN));
-        User user = new User();
-        user.setId(2);
-        user.setEmail("user@user.com");
-        user.setName("user1");
-        user.setVoted(false);
-        user.setRoles(Set.of(Role.USER));
-        return List.of(admin, user);
+    @Test
+    @WithMockUser(value = "admin@admin.com", password = "admin", roles = {"ADMIN"})
+    void whenUpdateAndUserNotFoundThenThrowException() throws Exception {
+        preform(MockMvcRequestBuilders.put("/admin/{id}", BAD_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(getUpdatedUserDto())))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(value = "admin@admin.com", password = "admin", roles = {"ADMIN"})
+    void whenUpdateWithBadEmailAndPasswordThenThrowException() throws Exception {
+        preform(MockMvcRequestBuilders.put("/admin/{id}", USER_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(getBadUserDto())))
+                .andExpect(status().isUnprocessableEntity());
     }
 }
