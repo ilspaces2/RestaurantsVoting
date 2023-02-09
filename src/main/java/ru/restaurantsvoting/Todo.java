831c9f28eb558180772262281637f58661fb1131
I_M <moskva.88@mail.ru>
2023-02-10 00:03:17 +0300

    test_9

--- src/main/java/ru/restaurantsvoting/Todo.java
+++ src/main/java/ru/restaurantsvoting/Todo.java
@@ -3,7 +3,7 @@ package ru.restaurantsvoting;
 public class Todo {
     /*
 TODO
-   1. restaurant test
+   1. restaurant test. When "save" then incorrect work (write AlreadyExists)
    2. think what add...
  */
 }
