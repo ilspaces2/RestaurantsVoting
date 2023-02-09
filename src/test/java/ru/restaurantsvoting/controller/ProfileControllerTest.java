799471ae2fef4c19d5f1cc32753be053e1ac3ea1
I_M <moskva.88@mail.ru>
2023-02-09 16:21:49 +0300

    ProfileControllerTest_add_tests.

---
+++ src/test/java/ru/restaurantsvoting/controller/ProfileControllerTest.java
@@ -0,0 +1,70 @@
+package ru.restaurantsvoting.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.restaurantsvoting.UserDtoTestData.getBadUserDto;
+import static ru.restaurantsvoting.UserDtoTestData.getUpdatedUserDto;
+import static ru.restaurantsvoting.UserTestData.*;
+
+class ProfileControllerTest extends AbstractControllerTest {
+
+    private final String profileUrl = "/profile";
+
+    @Test
+    void delete() throws Exception {
+        preform(MockMvcRequestBuilders.delete(profileUrl)
+                .header("Authorization", getJwtToken(user)))
+                .andExpect(status().isNoContent());
+        preform(MockMvcRequestBuilders.get(profileUrl, USER_ID)
+                .header("Authorization", getJwtToken(user)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void update() throws Exception {
+        preform(MockMvcRequestBuilders.put(profileUrl)
+                .header("Authorization", getJwtToken(user))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(getUpdatedUserDto())))
+                .andDo(print())
+                .andExpect(status().isNoContent())
+                .andExpect(content().string(objectMapper.writeValueAsString(getUpdatedUser())));
+
+    }
+
+    @Test
+    void whenUpdateWithBadEmailAndPasswordThenThrowException() throws Exception {
+        preform(MockMvcRequestBuilders.put(profileUrl)
+                .header("Authorization", getJwtToken(user))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(getBadUserDto())))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    void get() throws Exception {
+        preform(MockMvcRequestBuilders.get(profileUrl)
+                .header("Authorization", getJwtToken(user)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(user)));
+    }
+
+    @Test
+    void whenBadToken() throws Exception {
+        preform(MockMvcRequestBuilders.get(profileUrl)
+                .header("Authorization", "token without roles"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void whenIncorrectToken() throws Exception {
+        preform(MockMvcRequestBuilders.get(profileUrl)
+                .header("Authorization", getJwtToken(user) + "incorrect"))
+                .andExpect(status().isBadRequest());
+    }
+}
