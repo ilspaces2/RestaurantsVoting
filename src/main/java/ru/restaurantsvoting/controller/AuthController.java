f5a6bc4240b845555c4e921239de67559cce32ba
I_M <moskva.88@mail.ru>
2023-02-14 17:18:04 +0300

    AuthController_swagger_requestBody_apiResponse.

--- src/main/java/ru/restaurantsvoting/controller/AuthController.java
+++ src/main/java/ru/restaurantsvoting/controller/AuthController.java
@@ -1,25 +1,45 @@
 package ru.restaurantsvoting.controller;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.restaurantsvoting.security.jwt.JwtRequestLogin;
 import ru.restaurantsvoting.security.jwt.JwtResponse;
 import ru.restaurantsvoting.service.AuthService;
 
 @Tag(name = "Authentication", description = "Authentication in the application")
 @RestController
+@RequestMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping(value = "login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            required = true,
+            content = @Content(schema =
+            @Schema(example = """
+                    {
+                      "email": "string",
+                      "password": "string"
+                    }
+                    """)))
+    @ApiResponse(
+            responseCode = "200",
+            content = @Content(examples =
+            @ExampleObject(value = """
+                    {
+                      "type": "string",
+                      "accessToken": "string"
+                    }
+                    """)))
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public JwtResponse login(@RequestBody JwtRequestLogin authRequest) {
         return authService.login(authRequest);
